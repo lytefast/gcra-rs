9241e4b464a321d4ca7d6e64caca8517bc69c222
Sam Shih <shih@discordapp.com>
2024-05-28 20:44:14 +0000

    [rate-limiter] `&mut` not required for RateLimiter
    `dashmap` already handles mut concurrency, and we don't perform any other mutation on theRateLimiter` itself during these function call    s.
    
    https://github.com/lytefast/gcra-rs/issues/6

--- examples/rate_limiter.rs
+++ examples/rate_limiter.rs
@@ -6,7 +6,7 @@ const WORKER_SHARD_COUNT: usize = 2;
 #[tokio::main]
 async fn main() -> Result<(), GcraError> {
     let rate_limit = RateLimit::per_sec(2);
-    let mut rl = RateLimiter::with_shards(CACHE_CAPACITY, WORKER_SHARD_COUNT);
+    let rl = RateLimiter::with_shards(CACHE_CAPACITY, WORKER_SHARD_COUNT);
 
     rl.check("key", rate_limit.clone(), 1).await?;
     rl.check("key", rate_limit.clone(), 1).await?;
